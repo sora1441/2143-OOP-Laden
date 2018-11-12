8167725a8c931394dc6ab6754f8cb6361e4d58ba
sora1441 <43249102+sora1441@users.noreply.github.com>
2018-11-12 10:45:19 -0600

    Add files via upload

---
+++ A06/Counter.cpp
@@ -0,0 +1,152 @@
+/*
+Brent Laden
+Git:sora1441
+OOP 2143
+A05
+11-2-18
+*/
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include<cstdlib>
+
+using namespace std;
+
+class Counter {
+	sf::Text time;			// contains time variables
+	sf::Clock clock;
+	sf::Time start;
+	sf::Time end;
+	sf::Time elapsed;		// time gone by
+
+	int fontSize;
+	sf::Font font;			// font of clock
+	sf::Color fontColor;    // color of clock
+
+	sf::Vector2f clockLocation;		// location of clock
+
+	sf::Color backgroundColor;         // background color
+	sf::RectangleShape counterBackground; 
+
+public:
+
+	//input:	start and ending time
+	//purpose:	creates clock
+	Counter(int startTime, int endTime) {
+		fontSize = 50;
+
+		elapsed = clock.getElapsedTime();
+		start = sf::seconds(startTime);
+		end = sf::seconds(endTime);
+
+		if (!font.loadFromFile("arial.ttf"))
+		{
+			cout << "Couldn't find your text File";
+		}
+
+		counterBackground.setSize(sf::Vector2f(100.f, 100.f));
+		counterBackground.setFillColor(backgroundColor.White);
+		time.setFont(font);
+		time.setString("0");
+		time.setCharacterSize(fontSize);
+		time.setFillColor(fontColor.Green);
+		time.setPosition(sf::Vector2f(300.f, 300.f));
+		changeClockPosition(300.0, 300.0);  //calls function to center clock
+	}
+
+	//turns int timer to string
+	void setTime(int timer) {
+		string txt = to_string(timer);
+		time.setString(txt);
+	}
+
+	//input:	sf window
+	//purpose:	draws the counter and background
+	void drawCounter(sf::RenderWindow &window) {
+		window.draw(counterBackground);
+		window.draw(time);
+		
+	}
+
+	//to close when done
+	bool isFinished(int startTime, int endTime) {
+		elapsed = clock.getElapsedTime();
+		if (startTime < endTime) {
+			if (elapsed.asSeconds() >= endTime + 1)
+				return true;
+			else
+				return false;
+		}
+		else if (endTime < startTime) {
+			if (elapsed.asSeconds() <= endTime)
+				return true;
+			else
+				return false;
+		}
+	}
+
+	//input:	starting and ending time
+	//purpose:	whether to count up or down
+	void clockDirection(int startTime, int endTime) {
+		elapsed = clock.getElapsedTime();
+		if (startTime < endTime)
+			setTime(elapsed.asSeconds());
+		else if (startTime > endTime)
+			setTime(start.asSeconds() - elapsed.asSeconds() + 1);
+	}
+
+	void changeClockPosition(float X, float Y) {
+		time.setPosition(sf::Vector2f(X, Y));
+		counterBackground.setPosition(sf::Vector2f(X, Y));
+	}
+	//input: nothing
+	//purpose: moves clock and background
+	void moveClock() {
+		elapsed = clock.getElapsedTime();
+		int e = elapsed.asSeconds();
+
+
+		if (e <= 1) {
+			time.move(0.4f, 0.4f);
+			counterBackground.move(0.4f, 0.4f);
+		}
+		else if (e > 1 && e <= 5) {
+			time.move(-0.4f, -0.4f);
+			counterBackground.move(-0.4f, -0.4f);
+		}
+		else if (e > 5) {
+			time.move(0.4f, 0.2f);
+			counterBackground.move(0.4f, 0.2f);
+		}
+	}
+};
+
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(900, 900), "");
+
+	int start = 0;
+	int end = 5;
+
+	Counter C1(start, end);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		C1.clockDirection(start, end);
+		C1.moveClock();
+
+		window.clear();
+		C1.drawCounter(window);
+		window.display();
+
+		if (C1.isFinished(start, end))
+			window.close();
+	}
+	return 0;
+}
